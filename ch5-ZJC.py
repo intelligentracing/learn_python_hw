fe6b6a229275b79724bc9288615919d5c5adad3e
Andrewzengjiacheng <61656862+Andrewzengjiacheng@users.noreply.github.com>
2020-03-08 16:22:39 +0800

    Update ch5-ZJC.py

--- ch5-ZJC.py
+++ ch5-ZJC.py
@@ -2,12 +2,18 @@ import random
 #第二题
 x=int(input('请输入第一个年份：'))
 y=int(input('请输入第二个年份：'))
+w=0
+List=[]
 while x<=y :
     if x/4 == x//4 :
-        print(x)
-    else:
-        print('不是')
+        List.append(x)
     x=x+1
+print(List)
+while w<=len(List)-1:
+    if List[w]/100==List[w]//100 and List[w]/100/4!=List[w]//100//4:
+        List.remove(List[w])
+    w=w+1
+print(List)
 #第三题
 List1=[]
 for i in range(10):
