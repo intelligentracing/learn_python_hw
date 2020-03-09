fd0e96dae8ca95e882ec2d00c942ec19fc87b201
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-03-09 11:54:30 +0800

    Update ch5_ZLF.py

--- ch5_ZLF.py
+++ ch5_ZLF.py
@@ -1,45 +1,46 @@
 
-def how_many_lunar_years(start_year,end_year):
+def how_many_lunar_years():
+    start_year = int(input("输入起始年"))
+    end_year = int(input("输入结束年"))
     try:
         if start_year<0 or end_year<0 or end_year<start_year:
             print("输入正确的年份")
         else:
             x = start_year
             y = end_year
-            print("从{0}到{0}有：".format(x,y),end ="")
-            while True:
-                if x == y+1 :
-                    break
-                if x%4 == 0:
-                    print(x,end =",")
-                    x+=1
-                else:
-                    x+=1
+            print("从{0}到{1}有：".format(x,y),end ="")
+            for n in range(x,y+1):
+                if n%4 == 0 and n%100 != 0:
+                    print(n,end =",")
+                elif n%400 == 0:
+                    print(n,end=",")
+
             print("为闰年")
     except:
         print("错误")
 
 def random_number_in_order():
     import random
-    A = random.randint(0,100)
-    B = random.randint(0,100)
-    C = random.randint(0,100)
-    D = random.randint(0,100)
-    E = random.randint(0,100)
-    F = random.randint(0,100)
-    G = random.randint(0,100)
-    H = random.randint(0,100)
-    Y = random.randint(0,100)
-    J = random.randint(0,100)
-    K = random.randint(0,100)
+    A = B = C = D = E = F = G = H = Y = J = K = None
     List = [A,B,C,D,E,F,G,H,Y,J,K]
-    print(sorted(List))
+    List2= []
+    for Z in range(0,11):
+        List[Z] = random.randint(0,100)
+    for D in range(0,100):
+        for Z in range(0,11):
+            if List[Z] == D:
+                List2.append(List[Z])
+    print(List2)
+                
+            
+        
 
-random_number_in_order()
-
-how_many_lunar_years(2020,2040)
 
     
+random_number_in_order()
+
+how_many_lunar_years()
+
     
 
         
