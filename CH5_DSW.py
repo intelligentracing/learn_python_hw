0d8ffe95bd52d3e0f6c84e8bf772d42c54814cdc
SSSdengshuwen <61626503+SSSdengshuwen@users.noreply.github.com>
2020-03-09 21:21:53 +0800

    Create CH5_DSW.py

---
+++ CH5_DSW.py
@@ -0,0 +1,23 @@
+import random
+#第一题
+a=int(input("第一个年份"))
+b=int(input("第二个年份"))
+list=[]
+if a>b
+  print('This is a error')
+while a<=b:
+   if a/100!=a//100:
+     if  a/4==a//4:
+      list.append(a)
+      print[list]
+   if a/400==a//400
+     list.append(a)
+     print[list]
+#第二题
+import random 
+list=[]
+for i in range (10)
+   list.append(random.randint(0,101))
+print(list)
+list1=sorted(list)
+print(list1)
