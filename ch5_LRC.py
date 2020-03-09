1a785a4d02bb71401cf758f590de762dfd8b379b
HMTJYQS <13082207660@163.com>
2020-03-09 10:25:17 +0800

    Create ch5_LRC.py

---
+++ ch5_LRC.py
@@ -0,0 +1,13 @@
+for i in range(2000,2501):
+    if i%4==0 and i%100!=0 or i%400==0:
+ 
+        print(i)
+#-————————————————————分割线————————————————————-
+import random
+
+list_1 = []
+for i in range(10):
+	list_1.append(random.randint(0,100)
+print('生成的随机整数列表为：\n',list_1)
+list_2.sort()
+print('排序后的列表为：\n',list_1)
