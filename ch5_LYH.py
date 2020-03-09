2ccd7f5bf5acd92bb8511b66fbd6521e499a4c05
liuyihong2004 <61647991+liuyihong2004@users.noreply.github.com>
2020-03-09 12:10:38 +0800

    Update ch5_LYH.py

--- ch5_LYH.py
+++ ch5_LYH.py
@@ -3,17 +3,21 @@ leap_years=[]
 The_year1=input('please provide an integer limit to finding leap year:')
 The_year2=input('please provide an integer limit to finding leap year:')
 try:
-    The_year=int(The_year)
+    The_year1=int(The_year1)
+    The_year2=int(The_year2)
 except:
     print('Not a valid integer input. Exit')
 else:
-     for year1 in range(0,The_year+1):
-      if (year1 % 4 == 0 and year1 % 100 != 0) or year1 % 400 == 0:
-        leap_years.append(year1)
+     leap_years.append(year1)
         print(leap_years)
+     for year1 in range(0,The_year+1):
+      if year1 % 4 == 0 and year1 % 100 != 0 or year1 % 400 == 0:
+       
    
 #ex3
 import random
 num=random.sample(range(0,100),10)
 num.sort()
 print(num)
+
+
