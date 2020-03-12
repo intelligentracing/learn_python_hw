cebc5f380ab4017dd6cfe3222f83b3c845b5799d
HMTJYQS <61144951+HMTJYQS@users.noreply.github.com>
2020-03-12 16:55:10 +0800

    Update ch7_YQS.py

--- ch7_YQS.py
+++ ch7_YQS.py
@@ -1,82 +1,85 @@
-# def merge_sort(input_list,reverse = False):
-#     ''' Merge sort function using recursion
-#     Parameters:
-#     Input:  input_list  - a list of numerical numbers
-#
-#     Output: input_list  - sorted list
-#     '''
-#
-#     # Deploy Divide-and-Conquer
-#     if type(reverse)!=bool and type(input_list) != list:
-#         return False
-#     if reverse == False:
-#         if len(input_list) > 1:
-#             mid = len(input_list) // 2
-#             left_half = input_list[:mid]
-#             right_half = input_list[mid:]
-#
-#             # Recursively sort left and right sub-lists
-#             merge_sort(left_half)
-#             merge_sort(right_half)
-#
-#             # Merging left_half and right_half
-#             i = 0
-#             j = 0
-#             k = 0
-#             while i < len(left_half) and j < len(right_half):
-#                 if left_half[i] <= right_half[j]:
-#                     input_list[k] = left_half[i]
-#                     i = i + 1
-#                 else:
-#                     input_list[k] = right_half[j]
-#                     j = j + 1
-#                 k = k + 1
-#
-#             while i < len(left_half):
-#                 input_list[k] = left_half[i]
-#                 i = i + 1
-#                 k = k + 1
-#
-#             while j < len(right_half):
-#                 input_list[k] = right_half[j]
-#                 j = j + 1
-#                 k = k + 1
-#     elif reverse == True:
-#         if len(input_list) > 1:
-#             mid = len(input_list) // 2
-#             left_half = input_list[:mid]
-#             right_half = input_list[mid:]
-#
-#             # Recursively sort left and right sub-lists
-#             merge_sort(left_half,reverse=True)
-#             merge_sort(right_half,reverse=True)
-#
-#             # Merging left_half and right_half
-#             i = 0
-#             j = 0
-#             k = 0
-#             while i < len(left_half) and j < len(right_half):
-#                 if left_half[i] >= right_half[j]:
-#                     input_list[k] = left_half[i]
-#                     i = i + 1
-#                 else:
-#                     input_list[k] = right_half[j]
-#                     j = j + 1
-#                 k = k + 1
-#
-#             while i < len(left_half):
-#                 input_list[k] = left_half[i]
-#                 i = i + 1
-#                 k = k + 1
-#
-#             while j < len(right_half):
-#                 input_list[k] = right_half[j]
-#                 j = j + 1
-#                 k = k + 1
-#
-# list1 = 'string'
-# merge_sort(list1,reverse=False)
-# print(list1)
+ ex1.1
+    def merge_sort(input_list,reverse = False):
+     ''' Merge sort function using recursion
+     Parameters:
+     Input:  input_list  - a list of numerical numbers
+
+     Output: input_list  - sorted list
+     '''
+
+     # Deploy Divide-and-Conquer
+     if type(reverse)!=bool and type(input_list) != list:
+         return False
+     if reverse == False:
+         if len(input_list) > 1:
+             mid = len(input_list) // 2
+             left_half = input_list[:mid]
+             right_half = input_list[mid:]
+
+             # Recursively sort left and right sub-lists
+             merge_sort(left_half)
+             merge_sort(right_half)
+
+             # Merging left_half and right_half
+             i = 0
+             j = 0
+             k = 0
+             while i < len(left_half) and j < len(right_half):
+                 if left_half[i] <= right_half[j]:
+                     input_list[k] = left_half[i]
+                     i = i + 1
+                 else:
+                     input_list[k] = right_half[j]
+                     j = j + 1
+                 k = k + 1
+
+             while i < len(left_half):
+                 input_list[k] = left_half[i]
+                 i = i + 1
+                 k = k + 1
+
+             while j < len(right_half):
+                 input_list[k] = right_half[j]
+                 j = j + 1
+                 k = k + 1
+     elif reverse == True:
+         if len(input_list) > 1:
+             mid = len(input_list) // 2
+             left_half = input_list[:mid]
+             right_half = input_list[mid:]
+
+             # Recursively sort left and right sub-lists
+             merge_sort(left_half,reverse=True)
+             merge_sort(right_half,reverse=True)
+
+             # Merging left_half and right_half
+             i = 0
+             j = 0
+             k = 0
+             while i < len(left_half) and j < len(right_half):
+                 if left_half[i] >= right_half[j]:
+                     input_list[k] = left_half[i]
+                     i = i + 1
+                 else:
+                     input_list[k] = right_half[j]
+                     j = j + 1
+                 k = k + 1
+
+             while i < len(left_half):
+                 input_list[k] = left_half[i]
+                 i = i + 1
+                 k = k + 1
+
+             while j < len(right_half):
+                 input_list[k] = right_half[j]
+                 j = j + 1
+                 k = k + 1
+
+ list1 = 'string'
+ merge_sort(list1,reverse=False)
+ print(list1)
+
+#ex1.2
 def pascal_triangle(x,y):
     '''x:第几行
        y:第几个数
