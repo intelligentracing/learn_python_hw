0b6d1e484e7bcfa9cf31f1cf1bc6d11177658719
HMTJYQS <13082207660@163.com>
2020-03-12 16:48:22 +0800

    Create ch7_YQS.py

---
+++ ch7_YQS.py
@@ -0,0 +1,96 @@
+# def merge_sort(input_list,reverse = False):
+#     ''' Merge sort function using recursion
+#     Parameters:
+#     Input:  input_list  - a list of numerical numbers
+#
+#     Output: input_list  - sorted list
+#     '''
+#
+#     # Deploy Divide-and-Conquer
+#     if type(reverse)!=bool and type(input_list) != list:
+#         return False
+#     if reverse == False:
+#         if len(input_list) > 1:
+#             mid = len(input_list) // 2
+#             left_half = input_list[:mid]
+#             right_half = input_list[mid:]
+#
+#             # Recursively sort left and right sub-lists
+#             merge_sort(left_half)
+#             merge_sort(right_half)
+#
+#             # Merging left_half and right_half
+#             i = 0
+#             j = 0
+#             k = 0
+#             while i < len(left_half) and j < len(right_half):
+#                 if left_half[i] <= right_half[j]:
+#                     input_list[k] = left_half[i]
+#                     i = i + 1
+#                 else:
+#                     input_list[k] = right_half[j]
+#                     j = j + 1
+#                 k = k + 1
+#
+#             while i < len(left_half):
+#                 input_list[k] = left_half[i]
+#                 i = i + 1
+#                 k = k + 1
+#
+#             while j < len(right_half):
+#                 input_list[k] = right_half[j]
+#                 j = j + 1
+#                 k = k + 1
+#     elif reverse == True:
+#         if len(input_list) > 1:
+#             mid = len(input_list) // 2
+#             left_half = input_list[:mid]
+#             right_half = input_list[mid:]
+#
+#             # Recursively sort left and right sub-lists
+#             merge_sort(left_half,reverse=True)
+#             merge_sort(right_half,reverse=True)
+#
+#             # Merging left_half and right_half
+#             i = 0
+#             j = 0
+#             k = 0
+#             while i < len(left_half) and j < len(right_half):
+#                 if left_half[i] >= right_half[j]:
+#                     input_list[k] = left_half[i]
+#                     i = i + 1
+#                 else:
+#                     input_list[k] = right_half[j]
+#                     j = j + 1
+#                 k = k + 1
+#
+#             while i < len(left_half):
+#                 input_list[k] = left_half[i]
+#                 i = i + 1
+#                 k = k + 1
+#
+#             while j < len(right_half):
+#                 input_list[k] = right_half[j]
+#                 j = j + 1
+#                 k = k + 1
+#
+# list1 = 'string'
+# merge_sort(list1,reverse=False)
+# print(list1)
+def pascal_triangle(x,y):
+    '''x:第几行
+       y:第几个数
+       '''
+    if x == 1 and y == 1:
+        return 1
+    elif x < 1 or y < 1:
+        return 0
+    else:
+        return pascal_triangle(x - 1, y - 1) + pascal_triangle(x - 1, y)
+N = 11
+for x in range(1, N + 1):
+    for y in range(1, N - x + 1):
+        print("", end=' ')
+    for z in range(1, x + 1):
+        print(pascal_triangle(x, z),"",end=" ")
+    print()
