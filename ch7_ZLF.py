095cee96cd9e19546e024481529bc77f06122117
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-03-13 23:53:15 +0800

    Create ch7_ZLF.py

---
+++ ch7_ZLF.py
@@ -0,0 +1,84 @@
+import random
+import time
+
+
+def merge_sort(input_list,reverse=False):
+    ''' Merge sort function using recursion
+    Parameters:
+    Input:  input_list  - a list of numerical numbers
+    Output: input_list  - sorted list
+    '''
+    
+    # Deploy Divide-and-Conquer
+    if len(input_list)>1:
+        mid = len(input_list)//2
+        left_half = input_list[:mid]
+        right_half = input_list[mid:]
+
+        # Recursively sort left and right sub-lists
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merging left_half and right_half 
+        i=0
+        j=0
+        k=0
+        if reverse==True:
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] >= right_half[j]:
+                    input_list[k]=left_half[i]
+                    i=i+1
+                else:
+                    input_list[k]=right_half[j]
+                    j=j+1
+                k=k+1
+
+            while i < len(left_half):
+                input_list[k]=left_half[i]
+                i=i+1
+                k=k+1
+
+            while j < len(right_half):
+                input_list[k]=right_half[j]
+                j=j+1
+                k=k+1
+
+        if reverse!=True:
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] <= right_half[j]:
+                    input_list[k]=left_half[i]
+                    i=i+1
+                else:
+                    input_list[k]=right_half[j]
+                    j=j+1
+                k=k+1
+
+            while i < len(left_half):
+                input_list[k]=left_half[i]
+                i=i+1
+                k=k+1
+
+            while j < len(right_half):
+                input_list[k]=right_half[j]
+                j=j+1
+                k=k+1
+
+
+# Generate a sufficiently long list for sorting
+sample_count = 10000
+random_input = random.sample(range(0, sample_count),sample_count)
+
+
+
+# ******** Method 2: Merge Sort ********
+print('*** Merge Sort ***')
+result = random_input.copy()
+begin_time = time.time()
+merge_sort(result)
+
+# tic-toc 
+elapsed_time = time.time() - begin_time
+print(elapsed_time)
+print(result[0:20])
+
+
