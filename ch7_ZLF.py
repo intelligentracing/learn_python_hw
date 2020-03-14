e4c9b163ab3e6bb76c25a21db68abbbe1a0737db
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-03-14 10:18:00 +0800

    Update ch7_ZLF.py

--- ch7_ZLF.py
+++ ch7_ZLF.py
@@ -3,12 +3,7 @@ import time
 
 
 def merge_sort(input_list,reverse=False):
-    ''' Merge sort function using recursion
-    Parameters:
-    Input:  input_list  - a list of numerical numbers
-    Output: input_list  - sorted list
-    '''
-    
+ 
     # Deploy Divide-and-Conquer
     if len(input_list)>1:
         mid = len(input_list)//2
@@ -43,7 +38,7 @@ def merge_sort(input_list,reverse=False):
                 j=j+1
                 k=k+1
 
-        if reverse!=True:
+        else:
             while i < len(left_half) and j < len(right_half):
                 if left_half[i] <= right_half[j]:
                     input_list[k]=left_half[i]
@@ -80,5 +75,18 @@ merge_sort(result)
 elapsed_time = time.time() - begin_time
 print(elapsed_time)
 print(result[0:20])
+
+
+def generate(self, numRows: int) -> List[List[int]]:
+        if numRows==0:return []
+        triangle=[[1]]
+        if numRows==1: return triangle
+        for i in range(1,numRows):
+            tmp=[1]
+            for j in range(1,i):
+                tmp.append(triangle[i-1][j-1]+triangle[i-1][j])
+            tmp.append(1)
+            triangle.append(tmp)
+        return triangle
 
 
