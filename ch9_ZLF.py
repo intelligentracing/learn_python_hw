de6f5a1afd2e59727c6d0ea57c884133b058d501
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-03-18 11:53:45 +0800

    Update ch9_ZLF.py

--- ch9_ZLF.py
+++ ch9_ZLF.py
@@ -1,14 +1,15 @@
+
 from collections import deque
 
-def BFS(board_size, start, goal, legit_moves,blocks):
-    ''' BFS search a viable path from start position to goal position on the board_size
+def DFS(board_size, start, goal, legit_moves,blocks):
+    ''' DFS search a viable path from start position to goal position on the board_size
     Parameters:
     Input:  board_size  - The dimension of the board
             start       - start position of the piece
             goal        - final destination
             legit_moves - describe how the piece can move on the board
     
-    Output: result_path - return a BFS path that reaches the goal, otherwise []
+    Output: result_path - return a DFS path that reaches the goal, otherwise []
     '''
     # Input sanity check
     if len(board_size)!=2 or type(board_size[0])!=int or type(board_size[1])!=int:
@@ -27,17 +28,17 @@ def BFS(board_size, start, goal, legit_moves,blocks):
         raise ValueError('Start position value is not supported')
 
     # Initialization
-    search_queue = deque()    # this is a queue to manage the order of the BFS
-    search_queue.append(start)
+    search_stack = deque()    # this is a queue to manage the order of the DFS
+    search_stack.append(start)
 
     # move_parent records the parent notes of the searched moves on the board
-    move_parent  = [[[None,None] for i in range(board_size[1])] for j in range(board_size[0])]
-    move_parent[start[0]][start[1]] = [-1, -1]     # First root location is assigned a special value of (-1, -1)
+    parent_map  = [[[None,None] for i in range(board_size[1])] for j in range(board_size[0])]
+    parent_map[start[0]][start[1]] = [-1, -1]
     is_goal = False
-    while len(search_queue)>0:
+    while len(search_stack)>0 and not is_goal:
         
-        # Retrieve the current FIFO position
-        current_move = search_queue.popleft()
+        # Retrieve the current FILO position
+        current_move = search_stack.pop()
 
         # Generate all legit moves
         for i in legit_moves:
@@ -46,9 +47,10 @@ def BFS(board_size, start, goal, legit_moves,blocks):
             move_position = [ current_move[0] + i[0],current_move[1] + i[1]]
 
             # This move may be out of bound or have been visited
-            if move_position[0]<0 or move_position[1]<0 or move_position[0]>=board_size[0] or move_position[1]>=board_size[1]:
+            if move_position[0]<0 or move_position[1]<0 or move_position[0]>=board_size[0] \
+                or move_position[1]>=board_size[1]:
                 continue
-            elif move_parent[move_position[0]][move_position[1]]!=[None, None]:
+            elif parent_map[move_position[0]][move_position[1]]!=[None, None]:
                 continue
             out_of_range= False
             for n in blocks:
@@ -58,26 +60,27 @@ def BFS(board_size, start, goal, legit_moves,blocks):
                 continue
 
             # This is a valid position
-            search_queue.append(move_position)
-            move_parent[move_position[0]][move_position[1]] = current_move
+            search_stack.append(move_position)
+            parent_map[move_position[0]][move_position[1]] = current_move
 
             # Check if the new position is the goal
-            if move_position[0] == goal[0] and move_position[1] == goal[1]:
+            if move_position == goal:
                 is_goal = True
                 break
-        
-        if is_goal:
-            # Assign the found path and quit
-            path_queue = deque()
-
-            while is_goal:
-                path_queue.append(move_position)
-                move_position = move_parent[move_position[0]][move_position[1]]
-                if move_position[0]==-1:
-                    is_goal = False
+    no_path_deque = 'no available route'   
+    path_queue = deque()
+    if is_goal:
+        # Assign the found path and quit
+        while is_goal:
+            path_queue.appendleft(move_position)
+            move_position = parent_map[move_position[0]][move_position[1]]
+            if move_position[0]==-1:
+                is_goal = False
             
-            return path_queue
-
+    if path_queue != deque():
+        return path_queue
+    else:
+        return no_path_deque
 
 # Assign chess board size. Here half a standard board is used
 board_size = [4,8]
@@ -98,14 +101,15 @@ if board_size[1]>board_size[0]:
        bishop_moves.append([n*-1,n*-1])
        bishop_moves.append([n*-1,n])
        bishop_moves.append([n,n*-1])
-
 # Assign bishop's initial position
 bishop_start = [0,0]
+
+blocks=[[0,2]]
 
 # Create a display of the board game problem
 print('Game has started, here is the board with the initial position at 0')
 board_display = [[' * ' for i in range(board_size[1])] for j in range(board_size[0])]
-board_display[bishop_start[0]][bishop_start[1]] = ' 0 '
+board_display[bishop_start[0]][bishop_start[1]] = ' S '
 for i in range(board_size[0]):
     display_string = ''.join(board_display[i])
     print(display_string)
@@ -115,13 +119,22 @@ user_input = input('Please input goal position (x, y): ')
 bishop_goal = list(eval(user_input))
 
 print('Moving bishop from {0} to {1}:'.format(bishop_start, bishop_goal))
-board_display[bishop_goal[0]][bishop_goal[1]]  = ' 1 '
-for i in range(board_size[0]):
-    display_string = ''.join(board_display[i])
-    print(display_string)
+board_display[bishop_goal[0]][bishop_goal[1]]  = ' G '
 
-blocks=[[0,2]]
-
-bishop_path = BFS(board_size, bishop_start, bishop_goal, bishop_moves,blocks)
+bishop_path = DFS(board_size, bishop_start, bishop_goal, bishop_moves,blocks)
 
 print(bishop_path)
+try:
+    if len(bishop_path)>0:
+        start = bishop_path.popleft()
+        bishop_path.append(start)
+        for i in range(1,len(bishop_path)-1):
+            current_move = bishop_path.popleft()
+            board_display[current_move[0]][current_move[1]]  = ' o '
+            bishop_path.append(current_move)
+        
+    for i in range(board_size[0]):
+        display_string = ''.join(board_display[i])
+        print(display_string)
+except:
+    pass
