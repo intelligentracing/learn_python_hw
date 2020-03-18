458a33985b49a39beb0642275151eb7ca93bf8b1
intelligentracinginc <allen@intelligentracing.com>
2020-03-17 19:35:12 -0700

    Add reverse condition

--- merge_sort.py
+++ merge_sort.py
@@ -28,21 +28,23 @@ def merge_sort(input_list, reverse = False):
         right_half = input_list[mid:]
 
         # Recursively sort left and right sub-lists
-        merge_sort(left_half)
-        merge_sort(right_half)
+        merge_sort(left_half, reverse)
+        merge_sort(right_half, reverse)
 
         # Merging left_half and right_half 
         left_pointer=0
         right_pointer=0
         merged_pointer=0
         while left_pointer < len(left_half) and right_pointer < len(right_half):
-            
-            if left_half[left_pointer] <= right_half[right_pointer]:
+
+            if (left_half[left_pointer] <= right_half[right_pointer] and reverse == False) or \
+                (left_half[left_pointer] >= right_half[right_pointer] and reverse == True):
                 input_list[merged_pointer]=left_half[left_pointer]
                 left_pointer=left_pointer+1
             else:
                 input_list[merged_pointer]=right_half[right_pointer]
                 right_pointer=right_pointer+1
+            
             merged_pointer=merged_pointer+1
 
         while left_pointer < len(left_half):
@@ -62,12 +64,12 @@ random_input = random.sample(range(0, sample_count),sample_count)
 # Test a wrong parameter
 result = random_input.copy()
 
-merge_sort(result, False)
+# merge_sort(result, False)
 
 # ******** Method 2: Merge Sort ********
 print('*** Merge Sort ***')
 begin_time = time.time()
-merge_sort(result)
+merge_sort(result, True)
 
 # tic-toc 
 elapsed_time = time.time() - begin_time
