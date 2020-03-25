c158f3531938929e0a876855ab3a640a347453b5
DSW <1719532081@qq.com>
2020-03-25 13:28:16 +0800

    Update CH10_DSW.py

--- CH10_DSW.py
+++ CH10_DSW.py
@@ -1,9 +1,9 @@
 def hanoi (a,b,c,g):
-    g=int(input('please input the number of the layer of the hanoi'))
-  if g ==1:
-     print(a,"move to",c)
-  else:
-     hanoi(a,c,b,g-1)
-     print(a,"move to",c)
-     hanoi(b,a,c,g-1)
-    print def hanoi (a,b,c,g)
+     g=int(input('please input the number of the layer of the hanoi'))
+   if g ==1:
+      print(a,"move to",c)
+   else:
+      hanoi(a,c,b,g-1)
+      print(a,"move to",c)
+      hanoi(b,a,c,g-1)
+      print def hanoi (a,b,c,g) 
