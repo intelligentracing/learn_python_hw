b94a4bed9a572c899a3d4ecc6c1c4a0d675cf9f3
DSW <1719532081@qq.com>
2020-03-25 12:38:36 +0800

    Update CH11_DSW.py

--- CH11_DSW.py
+++ CH11_DSW.py
@@ -3,8 +3,7 @@ def optimal_cut(length):
     '''计算不同长度香肠最大卖多少钱
        length:香肠长度
        output:买最多的钱'''
-    if length<=0:
-        return 0
+    length  >1
     if optimal_cut_memory[length] == None:
         max_value=0
         for cut in range (length):
