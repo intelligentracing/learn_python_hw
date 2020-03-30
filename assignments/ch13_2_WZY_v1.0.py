5edc4bb33196455fe1cedbada090d655b10a4a60
Trance <wuzheyuan.86@gmail.com>
2020-03-30 11:53:01 +0800

    2020.3.30

--- assignments/ch13_2_WZY_v1.0.py
+++ assignments/ch13_2_WZY_v1.0.py
@@ -28,36 +28,6 @@ class Square(Shape):
     def get_area(self):
         ''' Area of a square is its width times width'''
         self.area = self.width*self.width
-        return self.area## This is course material for Introduction to Python Scientific Programming
-## Class 13 Example code: shape_class.py
-## Author: Allen Y. Yang,  Intelligent Racing Inc.
-##
-## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
-
-class Shape:
-    ''' An example class that defines an empty abstract shape '''
-    initiated = False
-
-    def get_area(self):
-        ''' virtual method to calculate area of a shape'''
-        pass
-
-    @classmethod
-    def is_init(cls):
-        ''' return True if a shape has been assigned to the class'''
-        return cls.initiated
-
-class Square(Shape):
-    ''' A subclass of Shape, specifically for calculating square area'''
-
-    def __init__(self, width):
-        self.width = width
-        self.get_area()
-        self.__class__.initiated = True      
-    
-    def get_area(self):
-        ''' Area of a square is its width times width'''
-        self.area = self.width*self.width
         return self.area
 
     def plus(self,other):
@@ -78,4 +48,4 @@ class Rectangle(Shape):
         self.area = self.width*self.length
         return self.area
 
-print (Square(3).plus(Square(3)).get_area)
+print ((Square(3).plus(Square(3))).area)
