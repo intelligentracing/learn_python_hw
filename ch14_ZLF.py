135c8307a768c6f067a1124a516446ac19950132
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-03-30 12:07:47 +0800

    Update ch14_ZLF.py

--- ch14_ZLF.py
+++ ch14_ZLF.py
@@ -18,28 +18,23 @@ class Rectangle(Shape):
         self.length = length
         self.get_area()
         self.__class__.initiated  = True
-
     def get_area(self):
         self.area = self.width*self.length
         return self.area
 
 class Square(Shape):
     ''' A subclass of Shape, specifically for calculating square area'''
-
     def __init__(self, width):
         self.width = width
         self.get_area()
         self.__class__.initiated = True  
-
     def __add__(self,other):
         if self.width == other.width:
-            print( Rectangle(self.width,self.width*2).area  )  
-    
+            return Rectangle(self.width,self.width*2).area  
     def get_area(self):
         ''' Area of a square is its width times width'''
         self.area = self.width*self.width
         return self.area
 
-
-Square(3) + Square(3)
+print(Square(3) + Square(3))
 
