62f15a6fedcd82a4e13419e234d0d91ba24a4f68
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-04-01 11:41:19 +0800

    Update ch14_ZLF.py

--- ch14_ZLF.py
+++ ch14_ZLF.py
@@ -1,18 +1,12 @@
 class Shape:
-    ''' An example class that defines an empty abstract shape '''
     initiated = False
-
     def get_area(self):
-        ''' virtual method to calculate area of a shape'''
         pass
-
     @classmethod
     def is_init(cls):
-        ''' return True if a shape has been assigned to the class'''
         return cls.initiated
 
 class Rectangle(Shape):
-
     def __init__(self,width,length):
         self.width = width
         self.length = length
@@ -23,7 +17,6 @@ class Rectangle(Shape):
         return self.area
 
 class Square(Shape):
-    ''' A subclass of Shape, specifically for calculating square area'''
     def __init__(self, width):
         self.width = width
         self.get_area()
@@ -32,9 +25,7 @@ class Square(Shape):
         if self.width == other.width:
             return Rectangle(self.width,self.width*2).area  
     def get_area(self):
-        ''' Area of a square is its width times width'''
         self.area = self.width*self.width
         return self.area
-
 print(Square(3) + Square(3))
 
