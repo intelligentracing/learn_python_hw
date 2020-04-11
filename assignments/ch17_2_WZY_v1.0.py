af07d0b5711ac4dedcaecdb0bf9403f3073f600a
Trance <wuzheyuan.86@gmail.com>
2020-04-11 13:14:04 +0800

    2020.4.11

--- assignments/ch17_2_WZY_v1.0.py
+++ assignments/ch17_2_WZY_v1.0.py
@@ -13,10 +13,8 @@ ax.spines['bottom'].set_position(('data', 0))
 ax.spines['top'].set_color('none')
 ax.spines['right'].set_color('none')
 
-# Initialize the cursor input
-cursor = Cursor(ax, horizOn = True, vertOn = True, color = 'green', linewidth=2.0)
+cursor = Cursor(ax, horizOn = True, vertOn = True, color = 'black', linewidth=1.0)
 
-# Initialize the A and b matrices
 A = np.zeros((2,2))
 b = np.ones(2)
 ob_count = 0
@@ -27,31 +25,32 @@ y_sample=[]
 def onclick(event):
     global ob_count, A, b, theta, x_sample,y_sample
 
-    if ob_count==11:
+    if ob_count == 10:
         quit()
 
     [x, y] = [event.xdata, event.ydata]
-    plt.plot(x, y, 'ro')
+    x_sample.append(x)
+    y_sample.append(y)
+    plt.plot(x, y, 'co')
+    ob_count += 1
 
-    # Update A and b
-    if x>-10 and x<10 and y>-10 and y<10:
-        x_sample.append(x**2);  y_sample.append(y**2)
-        ob_count +=1
-    
-    if ob_count==10:
+    if ob_count == 10:
+        x_sample_array = np.array(x_sample)
+        y_sample_array = np.array(y_sample)
         A = np.zeros((ob_count, 2))
         for i in range(ob_count):
-            A[i,0] = x_sample[i]
-            A[i,1] = 1.0
+            A[i, 0] = x_sample_array[i] * x_sample_array[i]
+            A[i, 1] = 1.0
+            y_sample_array[i] = y_sample_array[i]*y_sample_array[i]
 
-        aa = np.linalg.lstsq(A, np.array(y_sample), rcond = None)[0]
-
-        # Plot the ellipse
-        if aa[0]<=0 or aa[1]<=0: # a^2, b^2 must be positive
-            return
-        b = math.sqrt(aa[1]); a = math.sqrt(-1/(aa[0]/b**2))
-        x = a*np.cos(theta); y= b*np.sin(theta)
-        plt.plot(x,y,'k-', linewidth = 3)
+        aa = np.linalg.lstsq(A, y_sample_array, rcond=None)[0]
+        if aa[0] >= 0 or aa[1] <= 0: 
+            raise ValueError
+        a = math.sqrt(-aa[1]/aa[0])
+        b = math.sqrt(aa[1])
+        x1 = a * np.cos(theta)
+        y1 = b * np.sin(theta)
+        l2, = plt.plot(x1, y1, 'b', linewidth=3)
 
 fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
