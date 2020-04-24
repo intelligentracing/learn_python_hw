d23a1d48d6c56a7f355d08afb63ab9845db9cee9
HMTJYQS <13082207660@163.com>
2020-04-24 23:10:54 +0800

    Create ex5_Hashing.py

---
+++ ex5_Hashing.py
@@ -0,0 +1,19 @@
+import os
+from matplotlib import image
+import hashlib
+
+# Read an image file
+path = os.path.dirname(os.path.abspath(__file__))
+filename = path + '/' + 'lenna.bmp'
+filename1 = path + '/' + 'lenna_flag.jpg'
+hash_md5 = hashlib.md5()
+
+data = image.imread(filename)
+text = bytearray(data)
+hash_md5.update(text)
+print(hash_md5.hexdigest())
+
+data1 = image.imread(filename1)
+text1 = bytearray(data1)
+hash_md5.update(text1)
+print(hash_md5.hexdigest())
