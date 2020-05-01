d37ab1c100d939bc08a57054a79020c4037336f7
Black1149 <61324013+Black1149@users.noreply.github.com>
2020-05-01 10:46:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,2 @@
-
-## This is course material for Introduction to Python Scientific Programming
-## Class 15 Example code: rotation_dot_product.py
-## Author: Allen Y. Yang,  Intelligent Racing Inc.
-##
-## (c) Copyright 2020. Intelligent Racing Inc. Not permitted for commercial use
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-def rotationMatrix(degree):
-    c = np.cos(np.radians(degree))
-    s = np.sin(np.radians(degree))
-    return np.array([[c, -s], [s, c]])
-
-v1 = np.array([1.,3.])
-rotation_matrix = rotationMatrix(45)
-
-v2 = rotation_matrix.dot(v1)
-
-plt.arrow(0,0,v1[0],v1[1], head_width=0.8, head_length=0.8, color = 'b')
-plt.arrow(0,0,v2[0],v2[1],head_width=0.8, head_length=0.8, color = 'r')
-plt.axis([-5,5,0,10])
-plt.show()
+a = ['w']*50
+print(a)
