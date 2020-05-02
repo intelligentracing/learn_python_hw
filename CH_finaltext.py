dd31c5059343e317df60da5e333390765a8842b3
SSSdengshuwen <61626503+SSSdengshuwen@users.noreply.github.com>
2020-05-02 22:44:10 +0800

    Update CH_finaltext.py

---
+++ CH_finaltext.py
@@ -0,0 +1,18 @@
+import os
+from matplotlib import image
+import hashlib
+
+hash_md5 = hashlib.md5()
+path = os.path.dirname(os.path.abspath(__file__))
+filename = path + '/' + 'lenna.bmp'
+filename1 = path + '/' + 'lenna_flag.jpg'
+
+data = image.imread(filename)
+text = bytearray(data)
+hash_md5.update(text)
+print(hash_md5.hexdigest())
+
+data1 = image.imread(filename1)
+text1 = bytearray(data1)
+hash_md5.update(text1)
+print(hash_md5.hexdigest())
