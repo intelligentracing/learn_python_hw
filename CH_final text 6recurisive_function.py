e3f51cb3148344a117bc188a257edc6eb6bd0398
SSSdengshuwen <61626503+SSSdengshuwen@users.noreply.github.com>
2020-05-02 22:44:08 +0800

    Update CH_final text 6recurisive_function.py

--- CH_final text 6recurisive_function.py
+++ CH_final text 6recurisive_function.py
@@ -1,6 +1,16 @@
 from collections import deque
-def recursive_function(user_deque, reverse = True):
-    if user_deque == 1:
-        return 1 
-    else: 
-        return  user_deque + recursive_function(user_deque-1)
+def recursive_function(input_deque, reverse = False):
+
+    if type(input_deque) != deque or type(reverse) != bool:
+        raise TypeError('Input arguments are not allowed.')
+
+    if len(input_deque) == 1:
+        result = input_deque.pop()
+        print(result) 
+        return
+    input_deque.pop()
+    recursive_function(input_deque)
+
+
+input_deque = deque([1,2,3,4,5])
+recursive_function(input_deque)
