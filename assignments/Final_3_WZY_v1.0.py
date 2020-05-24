3dd4fbacb9bce0e65541928e001b0ba63ef089f4
Trance <wuzheyuan.86@gmail.com>
2020-05-24 11:12:06 +0800

    2020.5.24

--- assignments/Final_3_WZY_v1.0.py
+++ assignments/Final_3_WZY_v1.0.py
@@ -7,26 +7,42 @@
 # national flag image, and display the modified lenna image. Note that 
 # after the modification, the bigger image of lenna must include the full 
 # image of the smaller national flag image.
+
+#需要使用的库
 from matplotlib import image
 from matplotlib import pyplot
 import os
-# Read an image file
+
+# 读取文件信息
 path = os.path.dirname(os.path.abspath(__file__))
+#lenna文件
 lenna = path + '/' + 'lenna.bmp'
+#国旗文件
 flag= path + '/' + 'china-flag-xs.jpg'
+
+#读取lenna并显示
 data1 = image.imread(lenna)
 pyplot.imshow(data1)
 pyplot.show()
+
+#读取文件并显示
 data2 = image.imread(flag)
 pyplot.imshow(data2)
 pyplot.show()
-# Add some color boundaries to modify an image array
+
+#复制lenna文件信息
 plot_data = data1.copy()
+
+#国旗的高与宽
 h=167
 w=250
+
+#在lenna图片范围内(512*512)
 for width in range(512-250,512):
     for height in range(167):
+        #将lenna中的像素改为国旗中的像素
         plot_data[height][width] = [data2[height][width-512+250][0], data2[height][width-512+250][1], data2[height][width-512+250][2]]
-# use pyplot to plot the image
+
+#展示图像
 pyplot.imshow(plot_data)
 pyplot.show()
