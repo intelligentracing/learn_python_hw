8dfd7951da4c1158cd5ef8e03e76f193e2ebc42e
Trance <wuzheyuan.86@gmail.com>
2020-05-29 22:12:15 +0800

    2020.5.29

---
+++ assignments.II/ch1_1_WZY_v1.0.py
@@ -0,0 +1,56 @@
+# Ex1 使用 Canny 算法中的 5x5 的 blur kernel，利用 cv2.filter2D, 模糊 
+# 化处理我们提供的highway_video.mp4, 并将结果存在一个输出的AVI 文件中
+
+import cv2
+import numpy as np
+import os
+
+# load file
+path = os.path.dirname(os.path.abspath(__file__))
+filename = path + '/highway_video.mp4'
+outputFilename = path + '/output_video.avi'
+
+grabber = cv2.VideoCapture(filename)
+fps = int(grabber.get(cv2.CAP_PROP_FPS))
+width = int(grabber.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(grabber.get(cv2.CAP_PROP_FRAME_HEIGHT))
+output = cv2.VideoWriter(outputFilename, cv2.VideoWriter_fourcc('M', 'J', 'P','G'),\
+     fps, (width, height), False)
+
+def region_of_interest(image, height= None, width = None):
+    if image is None:
+        if (height is None) or (width is None):
+            return None
+    else:
+        height,width = image.shape
+
+    ROI = np.array([(0, height-100), (width, height-100), (width, height-300), (0, height-300)])
+    mask = np.zeros([height, width], dtype = np.uint8)
+    cv2.fillPoly(mask, [ROI], 255)
+    return mask
+
+mask_image = region_of_interest(None, height, width)
+
+while(grabber.isOpened()):
+    ret, frame = grabber.read()
+
+    if ret == False:
+        break
+
+    # Create gray image and denoise
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #edge_image = extract_edge(gray_image)
+    edge_image = cv2.Canny(gray_image, 100, 200, L2gradient = True)
+    ROI_image = cv2.bitwise_and(edge_image,mask_image)
+
+    cv2.imshow('Edge Image', ROI_image)
+    output.write(ROI_image)
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+
+grabber.release()
+output.release()
+cv2.destroyAllWindows()
---
+++ assignments.II/ch1_2_WZY_v1.0.py
@@ -0,0 +1,2 @@
+# Ex2 修改 floodfill 代码的 29 行，添加有关 mask 的一个用途。请参 照关于 floorfill 中的 mask 使用说明
+
