93c695bc151f49afd43806ee9439371cdadeaa3b
HMTJYQS <13082207660@163.com>
2020-06-25 13:17:53 +0800

    Create ex6_Recursive_Function.py

---
+++ learn_Python assignment/ex6_Recursive_Function.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def recursive_function(input_deque, reverse = False):
+
+    if type(input_deque) != deque or type(reverse) != bool:
+        raise TypeError('Input arguments are not allowed.')
+
+    if len(input_deque) == 1:
+        result = input_deque.pop()
+        print(result) 
+        return
+    input_deque.pop()
+    recursive_function(input_deque)
+
+
+input_deque = deque([1,2,3,4,5])
+recursive_function(input_deque)
