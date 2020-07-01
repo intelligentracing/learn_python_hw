8e74c5ee476a2266e6e2067aeb91cefe291f85db
Ryan Ren <ryan.ren@ucc.on.ca>
2020-06-30 23:22:57 -0400

    <message>

--- lecture5_hw1.py
+++ lecture5_hw1.py
@@ -1,14 +1,20 @@
 # This is the first problem I need to do for this weeks homework 
-#Essentially I need to output the leap years inbetween the 2 years that the user inputs 
+# Essentially I need to output the leap years inbetween the 2 years that the user inputs 
 
 year1 = input("Please input the first year:") 
 year2 = input("Please input the second year:") 
+def leapyear():
+    if year2 < year1: 
+        print("Sorry, unfortunately your first year has to smaller than your second year.")
+        return 
+    for i in range(int(year1)+1, int(year2)):
+        if i % 100 == 0:
+            if i % 400 == 0: 
+                print(i, end=" ")
+            else: 
+                continue  
+        elif i % 4 == 0:
+            print(i, end=" ")     
+    return  
 
-for i in range(int(year1)+1, int(year2)):
-    if i % 100 == 0:
-        if i % 400 == 0: 
-            print(i, end=" ")
-        else: 
-            continue  
-    elif i % 4 == 0:
-        print(i, end=" ")     
+leapyear() 
--- lecture5_hw2.py
+++ lecture5_hw2.py
@@ -1,18 +1,26 @@
 # This is the second problem that I need to solve for this week's homework
+# This question asks us to sort in ascending orders random integers from 1-100
 
+# This is the first solution using .sort() method 
 import random 
 
-random_integers = [] 
-
-for i in range(10): 
-    random_int = random.randint(1, 100) 
-    random_integers.append(random_int)
-    if i == 0:
-        continue 
-    elif random_int > random_integers[0:i]:
-        continue 
-    elif 
-
-print(random_integers)
 
 
+def sort_random_list():
+    random_list = random.sample(range(0, 100), 10)
+    random_list.sort()  
+    print(random_list)
+    return 
+
+sort_random_list() 
+
+# This is the second method using sorted()- With this way we have to assign sorted() to a variable then we
+# need to print the variable 
+
+def sort_random_list2():
+    random_list2 = random.sample(range(0, 100), 10)
+    a = sorted(random_list2)  
+    print(a)
+    return 
+
+sort_random_list2() 
