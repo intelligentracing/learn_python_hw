a7601e2f90f02b6cdbf5288e32f51cec9c14fbb1
HMTJYQS <13082207660@163.com>
2020-07-03 08:24:56 +0800

    Create ch4_YQS.py

---
+++ learn_Python assignment/ch4_YQS.py
@@ -0,0 +1,37 @@
+#ex1.1(1)
+# L1 = [['a', 'b'], 2, ['a','b']]
+# L = ['a', 'b']
+# L2 = [L, 2, L]
+
+# print('L1[0]: ', L1[0])
+# print('L1[2]: ', L1[2])
+
+# print('L2[0]: ', L2[0])
+# print('L2[2]: ', L2[2])
+# #ex1.1(2)
+# print(id(L))
+# L1[0][0] = 'c'
+# L2[0][0] = 'c'
+
+# print('L1: ', L1)
+# print('L2: ', L2)
+# print('L:', L)
+# print(id(L))
+
+#ex1.2
+list_1 = ['a','b','c', 4]
+list_2 = ['d','e','f']
+# list_1_1 = list_1[:]
+# list_1.append(list_2)
+# print(list_1)
+# print(len(list_1))
+# list_1_1.extend(list_2)
+# print(list_1_1)
+# print(len(list_1_1))
+
+list_3 = []
+for i in list_1:
+    list_3 += str(i)
+print(list_3)
+result = ' '.join(list_3)
+print(result)
