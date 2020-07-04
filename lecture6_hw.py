aa50bd9c46aece0103de6a38217471e44c53734a
M-Panda <44418582+M-Panda@users.noreply.github.com>
2020-07-03 20:18:34 -0500

    Add files via upload

---
+++ lecture6_hw.py
@@ -0,0 +1,38 @@
+#Ex1.1
+import math
+
+# ********  Case 1: Wrong text formatting  ********
+for i in range(10):        # Error
+    print(i)
+else:                        # Error
+      pass
+
+# ********  Case 2: Misuse of brackets  ********
+List = ['abc','def']        
+List[1] =  List[0]             # Error
+
+# ********  Case 3: Wrong indentation  ********
+x = 0
+y = 1
+z = 3
+
+# ********  Case 4: Forget to import modules  ********
+print(math.sqrt(9))
+
+# ********  Case 5: Call methods in the wrong type of objects  ********
+String = "Hello World!" [::-1]
+print(String)
+
+# ********  Case 6: Use the wrong operator on variable types
+List = ['abcde']
+List = List.append('abcde')
+
+# ********  Case 7: Misspell a variable
+animals = ['dog', 'cat', 'pony', 'fish', 'leopard', 'rabbit','mouse']
+print(animals[0])
+
+# ********  Case 8: Wrong use of quotation marks
+wrong_string = "Mike's story"
+print(wrong_string)
+
+#Ex1.2
