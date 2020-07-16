2976673b79e8c58ec6ea55e3ec2fedbbd85e83b2
Trance <wuzheyuan.86@gmail.com>
2020-07-16 15:27:50 +0800

    2020.7.16

--- Notes/class_17/cartpole_dqn.py
+++ Notes/class_17/cartpole_dqn.py
@@ -17,19 +17,23 @@ from keras.optimizers import Adam
 EPISODES = 1000
 
 class DQNAgent:
+    #模型初始化
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
+        #折扣
+        self.gamma = 0.95 
+        #探索速度
+        self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
-
+#制作模型
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
+        #2层神经元
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu'))
         model.add(Dense(24, activation='relu'))
@@ -37,42 +41,42 @@ class DQNAgent:
         model.compile(loss='mse',
                       optimizer=Adam(lr=self.learning_rate))
         return model
-
+#记录数据
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
-
+#回复行动
     def act(self, state):
+        #若数据过少
         if np.random.rand() <= self.epsilon:
+            #随机选择
             return random.randrange(self.action_size)
+        #预测
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])  # returns action
-
+#重玩/记录数据
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.model.predict(next_state)[0]))
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-
+#载入数据
     def load(self, name):
         self.model.load_weights(name)
-
+#导出数据
     def save(self, name):
         self.model.save_weights(name)
 
-histoscore=0.0
-lastavg=0.0
-avg=0.0
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
+    env = gym.make('CartPole-v0')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
+    #操作智能
     agent = DQNAgent(state_size, action_size)
     # agent.load("./save/cartpole-dqn.h5")
     done = False
@@ -90,12 +94,8 @@ if __name__ == "__main__":
             next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
             state = next_state
-            lastavg=avg
-            histoscore=histoscore+time
-            avg=histoscore/(e+1)
-            improvement=avg-lastavg
             if done:
-                print("episode: {}/{}, score: {}, e: {:.5}, average score: {:.5}, have improved: {:.5}".format(e, EPISODES, time, agent.epsilon, avg, improvement))
+                print("episode: {}/{}, score: {}, e: {}".format(e, EPISODES, time, agent.epsilon))
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
