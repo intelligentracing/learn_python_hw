d04610ec323d68204a26e23fb017b53192a6c0cc
HMTJYQS <13082207660@163.com>
2020-07-28 22:50:54 +0800

    Update ex4_Classes_and_Objects.py

--- learn_Python assignment/ex4_Classes_and_Objects.py
+++ learn_Python assignment/ex4_Classes_and_Objects.py
@@ -5,18 +5,22 @@ class Vehicle:
     def __init__(self, brand, model):
             self.brand = str(brand)
             self.model = str(model)
-            self.fuel_level = float(0)
-
+            self.fuel_level = None
+            if self.type not in type:
+                print('type', self.type, 'is not existed.')
+                self.type = 'sedan'
     def check_fuel_level(self,fuel_level):
         self.fuel_level = fuel_level
-        self.check_fuel_level = self.fuel_level / 100
-        return self.check_fuel_level, '%'
+        return self.fuel_level
 
-    def set_fuel_level(self):
-        set_level = input('fuel_level by percentage:')
-        self.set_level = set_level
-        return self.set_level
-
+    def set_fuel_level(self,fuel_level):
+        if  0 <= fuel_level <= 1:
+            self.fuel_level = fuel_level
+            return self.set_level
+        else:
+            print('the format of fuel level is wrong, fuel will be set to 0.5')
+            return self.fuel_level = 0.5
+            
 class ElectricVehicle(Vehicle):
 
     def __init__(self, brand, model):
