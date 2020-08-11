376bbe9bd97941fa8014e2e93f92d91a30f0aa68
HMTJYQS <13082207660@163.com>
2020-08-11 22:23:10 +0800

    Update ex5_Hashing.py

--- learn_Python assignment/ex5_Hashing.py
+++ learn_Python assignment/ex5_Hashing.py
@@ -6,15 +6,17 @@ import hashlib
 path = os.path.dirname(os.path.abspath(__file__))
 filename = path + '/' + 'lenna.bmp'
 filename1 = path + '/' + 'lenna_flag.jpg'
-hash_md5 = hashlib.md5()
-
 data = image.imread(filename)
+
+
+hash_md5 = hashlib.md5()
 text = bytearray(data)
 hash_md5.update(text)
 print(hash_md5.hexdigest())
 
 hash_md5_1 = hashlib.md5()
+print(type(hash_md5_1))
 data1 = image.imread(filename1)
 text1 = bytearray(data1)
 hash_md5_1.update(text1)
-print(hash_md5.hexdigest())
+print(hash_md5_1.hexdigest())
