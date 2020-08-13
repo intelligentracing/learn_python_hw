6d843920bbab0febbb63e8e4326fcf1f0f3b656c
HMTJYQS <13082207660@163.com>
2020-08-13 11:43:44 +0800

    Update ex5_Hashing.py

--- learn_Python assignment/ex5_Hashing.py
+++ learn_Python assignment/ex5_Hashing.py
@@ -14,9 +14,8 @@ text = bytearray(data)
 hash_md5.update(text)
 print(hash_md5.hexdigest())
 
-hash_md5_1 = hashlib.md5()
-print(type(hash_md5_1))
-data1 = image.imread(filename1)
+hash_md5_1 = hashlib.md5()#Gets an MD5 encryption algorithm object
+data1 = image.imread(filename1)#
 text1 = bytearray(data1)
-hash_md5_1.update(text1)
-print(hash_md5_1.hexdigest())
+hash_md5_1.update(text1)#Specify a byte array type data that needs to be encrypted
+print(hash_md5_1.hexdigest())#Gets the encrypted hexadecimal string
