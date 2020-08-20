0234f9ed3f9f46a56b9c232f65f751d26fca55ee
HMTJYQS <13082207660@163.com>
2020-08-20 22:54:37 +0800

    Update ch5_YQS.py

--- learn_Python assignment/ch5_YQS.py
+++ learn_Python assignment/ch5_YQS.py
@@ -1,24 +1,24 @@
 import random
 #ex1.2
-def output_years():
-    years_list = []
-    years1 = input('第一个年份：')
-    years2 = input('第二个年份：')
-    int_years1 = int(years1)
-    int_years2 = int(years2)
+# def output_years():
+#     years_list = []
+#     years1 = input('第一个年份：')
+#     years2 = input('第二个年份：')
+#     int_years1 = int(years1)
+#     int_years2 = int(years2)
 
-    if years2 < years1:
-        print('错误！第二个年份应大于第一个年份，请重新输入')
-        return
-    for years in range(int_years1,int_years2+1):
-        if years % 100 == 0:
-            if years % 400 == 0:
-                years_list.append(years)
-        elif years % 4 == 0:
-            years_list.append(years)
-    print(years_list)
-    return
-output_years()
+#     if years2 < years1:
+#         print('错误！第二个年份应大于第一个年份，请重新输入')
+#         return
+#     for years in range(int_years1,int_years2+1):
+#         if years % 100 == 0:
+#             if years % 400 == 0:
+#                 years_list.append(years)
+#         elif years % 4 == 0:
+#             years_list.append(years)
+#     print(years_list)
+#     return
+# output_years()
 #ex1.3
 def random_sort():
     num = range(0,100)
