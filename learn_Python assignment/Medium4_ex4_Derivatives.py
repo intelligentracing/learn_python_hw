9404a15ee92b32c7f4a4bbea3397ff3db6f535dd
HMTJYQS <13082207660@163.com>
2020-08-26 07:42:41 +0800

    Update Medium4_ex4_Derivatives.py

--- learn_Python assignment/Medium4_ex4_Derivatives.py
+++ learn_Python assignment/Medium4_ex4_Derivatives.py
@@ -1,2 +1,7 @@
-def func(x):
-    return
+def factorial(x):
+    factorial = 1
+    for i in range(1,x):
+        factorial = factorial * i
+    return factorial
+def sin(x):
+    return x - x**3/factorial(3) +
