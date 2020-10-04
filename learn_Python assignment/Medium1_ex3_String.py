e300bda21c2f26a185212e69a3c95c158820d293
HMTJYQS <13082207660@163.com>
2020-10-04 16:11:13 +0800

    Update Medium1_ex3_String.py

--- learn_Python assignment/Medium1_ex3_String.py
+++ learn_Python assignment/Medium1_ex3_String.py
@@ -1,3 +1,6 @@
+#Midium1 ex3_String.py
+#Author: Yu Qiuhsuang
+
 orign = input('please input the orign sentence:')
 test = input('please input the test sentence:')
 
@@ -7,9 +10,12 @@ orign = [s.lower() for s in orign if isinstance(s,str)==True]
 test = test.replace(' ','')
 test = sorted(test)
 test = [s.lower() for s in test if isinstance(s,str)==True]
-
+# test_result = []
+# for s in test:
+#     if isinstance(s, str) == True:
+#         test_result.append(s.lower()) 
 all = [x for x in orign if x in test]
-result = [y for y in (orign + test) if y not in all]
+result = [y for y in test if y not in all]
 
 if len(result) == 0:
     is_anagram = True
