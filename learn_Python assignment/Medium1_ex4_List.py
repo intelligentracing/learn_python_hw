89155ab9429315f2e54d6447ab541744cddeebf6
HMTJYQS <13082207660@163.com>
2020-10-09 14:15:56 +0800

    Update Medium1_ex4_List.py

--- learn_Python assignment/Medium1_ex4_List.py
+++ learn_Python assignment/Medium1_ex4_List.py
@@ -10,11 +10,10 @@ for i in test_list:
     result_list.append(int(i))
 
 n = len(result_list)
+shift = shift % n
 result_list_copy = result_list.copy()
 for s in range(n):
-    if s + shift < 0:
-        result_list_copy[n + s + shift] = result_list[s]
-    if n > s + shift >= 0:
+    if s + shift < n:
         result_list_copy[s + shift] = result_list[s]
     if s + shift >= n:
         result_list_copy[s + shift - n] = result_list[s]
