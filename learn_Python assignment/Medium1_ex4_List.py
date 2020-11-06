b74fda0a75c5c8f18c258e4bcd1199cf7042fe63
HMTJYQS <13082207660@163.com>
2020-11-06 20:56:41 +0800

    Update Medium1_ex4_List.py

--- learn_Python assignment/Medium1_ex4_List.py
+++ learn_Python assignment/Medium1_ex4_List.py
@@ -1,14 +1,19 @@
 #Midium1 ex4_List.py
 #Author: Yu Qiuhsuang
 
+#input interface
 element = input('please input the number for the list:')
 shift = input('please input shift steps:')
 shift = int(shift)
 test_list = list(element)
+
+#define a list to save the 
 result_list = []
 for i in test_list:
     result_list.append(int(i))
 
+print(result_list)
+print(test_list)
 n = len(result_list)
 shift = shift % n
 result_list_copy = result_list.copy()
