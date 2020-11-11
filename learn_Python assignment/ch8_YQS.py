b8b028b4bd2f59e5ab891e59822d89a9b8826ee4
HMTJYQS <13082207660@163.com>
2020-11-11 09:49:51 +0800

    Update ch8_YQS.py

--- learn_Python assignment/ch8_YQS.py
+++ learn_Python assignment/ch8_YQS.py
@@ -1,38 +1,6 @@
 from collections import deque
 
-#ex1.1
-# def find_flag(opt_queue,num):
-#     """找到num在已经排好序的opt_queue里应该放在什么位置，即flag"""
-#     flag = 0
-#     for i in range(len(opt_queue)):
-#         com = opt_queue.pop()
-#         if com > num:
-#             flag = i+1 #flag代表比几个数小 有flag数比较大
-#         opt_queue.append(com)
-#     return flag
 
-# def insert(opt_queue,flag,num):
-#     """把num插入opt_queue中"""
-#     new_queue=deque()
-#     for i in range(flag):
-#         new_queue.append(opt_queue.pop())
-#     opt_queue.append(num)
-#     for i in range(flag):
-#         opt_queue.append(new_queue.pop())
-#     return opt_queue
-
-# def sort_function(queue):
-#     """循环以上操作将未排序的queue中的每一个元素插入到在opt_queue这个已经排好序的列表中"""
-#     opt_queue = deque()
-
-#     for i in range(len(queue)):
-#         num = queue.popleft()
-#         flag = find_flag(opt_queue,num)
-#         opt_queue = insert(opt_queue, flag, num)
-#     return opt_queue
-
-# queue_1 = deque([2,10,1])
-# print(sort_function(queue_1))
 
 #ex1.2
 # def find_min_index(queue, remain):
@@ -284,6 +252,7 @@ knight_start = [0, 0]
 print('Game has started, here is the board with the initial position at 0')
 board_display = [[' * ' for i in range(board_size[1])] for j in range(board_size[0])]
 board_display[knight_start[0]][knight_start[1]] = ' S '
+#display the obstacle
 for i in range(2,6):
     for j in range(2,4):
         board_display[j][i] = ' x '
