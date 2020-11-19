f9d4ac637e3074eb7e047596ff3df9699a3e332d
HMTJYQS <13082207660@163.com>
2020-11-19 10:40:51 +0800

    Update Medium2_ex3_Recursive_Function.py

--- learn_Python assignment/Medium2_ex3_Recursive_Function.py
+++ learn_Python assignment/Medium2_ex3_Recursive_Function.py
@@ -3,18 +3,19 @@
 
 from collections import deque
 
-def recursive_function(input_deque, reverse = False):
+def dequeue(input_deque, reverse = False):
 
     if type(input_deque) != deque or type(reverse) != bool:
         raise TypeError('Input arguments are not allowed.')
-
+    
+    middle_queue = deque()
     if len(input_deque) == 1:
         result = input_deque.pop()
-        print(result) 
-        return
-    input_deque.pop()
-    recursive_function(input_deque)
+        return result
+
+    middle_queue.append(input_deque.pop())
+    dequeue(input_deque)
 
 
 input_deque = deque([1,2,3,4,5])
-recursive_function(input_deque)
+print(dequeue(input_deque))
