8c58f8488e5c0d2fa1bd0044bed166864eb2e7b9
HMTJYQS <13082207660@163.com>
2020-11-22 19:11:58 +0800

    Update Medium2_ex2_Global_variables.py

--- learn_Python assignment/Medium2_ex2_Global_variables.py
+++ learn_Python assignment/Medium2_ex2_Global_variables.py
@@ -8,7 +8,9 @@ def encode(string):
     user_input = deque(string)
     for i in range(len(user_input)):
         original = user_input.popleft()
-        result = chr( ord(original) + encryption)
+        #ord()function to convert a single character string to its corresponding ASCII code
+        #chr() function to convert the ASCII code back to a single character.
+        result = chr(ord(original) + encryption)
         user_input.append(result)
     user_input = ''.join(user_input)
     return user_input
