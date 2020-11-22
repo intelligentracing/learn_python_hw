4eb0d48299cd6ecf56b11b5b0e7e27a83c21e423
HMTJYQS <13082207660@163.com>
2020-11-22 19:11:51 +0800

    Update ch10_YQS.py

--- learn_Python assignment/ch10_YQS.py
+++ learn_Python assignment/ch10_YQS.py
@@ -24,22 +24,22 @@ def DP_num(num):
 print(DP_num(675))
 
 #ex1.2
-# price = [3,5,8,9,10,17,17,20]
-# n = 8
+price = [3,5,8,9,10,17,17,20]
+n = 8
 
-# def solution(price, n):
-#     """Apply subproblem optimality to solve the problem of how best to cut
-#         price：It's a list of prices sold for different meters
-#         n:It's an int, how many meters are there in this sausage
-#     """
-#     dp = [0] * n
-#     for i in range(n):      #start at 1 meter, and calculate the highest selling price
-#         dp[i] = price[i]    #The price when i meters are sold as a whole
-#         for j in range(i):  
-#             #When i meters are sold separately。The key: Turn a big problem into a small problem, 
-#             #and the optimal solution to the small problem is also the optimal solution to the big problem.
-#             if dp[i - j-1] + dp[j] > dp[i]: #Compare that to selling as a whole
-#                 dp[i] = dp[i-j-1] + dp[j]
-#     return dp[-1]
+def solution(price, n):
+    """Apply subproblem optimality to solve the problem of how best to cut
+        price：It's a list of prices sold for different meters
+        n:It's an int, how many meters are there in this sausage
+    """
+    dp = [0] * n
+    for i in range(n):      #start at 1 meter, and calculate the highest selling price
+        dp[i] = price[i]    #The price when i meters are sold as a whole
+        for j in range(i):  
+            #When i meters are sold separately。The key: Turn a big problem into a small problem, 
+            #and the optimal solution to the small problem is also the optimal solution to the big problem.
+            if dp[i - j-1] + dp[j] > dp[i]: #Compare that to selling as a whole
+                dp[i] = dp[i-j-1] + dp[j]
+    return dp[-1]
 
-# print(solution(price,n))
+print(solution(price,n))
