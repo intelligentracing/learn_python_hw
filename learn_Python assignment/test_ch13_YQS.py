8aa05dd9bbe5d7d3e1cda4b70833bd9826bffe1c
HMTJYQS <13082207660@163.com>
2020-11-29 20:17:43 +0800

    Update test_ch13_YQS.py

--- learn_Python assignment/test_ch13_YQS.py
+++ learn_Python assignment/test_ch13_YQS.py
@@ -1,8 +1,22 @@
-from ch13_YQS import Square
+# from ch13_YQS import Square
 
-s1 = Square(8)
-s2 = Square(8)
+# s1 = Square(8)
+# s2 = Square(8)
 
-enroll = s1 + s2
-print(s1.get_area())
-print(enroll.get_area())
+# enroll = s1 + s2
+# print(s1.get_area())
+# print(enroll.get_area())
+
+def appendTail(orign, value):
+    orign.append(value)
+    return orign
+
+def deleteHead(orign, value):
+    stack = []
+    while orign:
+        stack.append(orign.pop())
+    stack.pop()
+    while stack:
+        orign.append(stack.pop())
+    
+    return orign
