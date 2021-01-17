88884a4dfeafe03c70336b4e7f2b97a4be6cd664
HMTJYQS <13082207660@163.com>
2021-01-17 16:45:05 +0800

    Update Medium3_ex5_word_dictionary.py

--- learn_Python assignment/Medium3_ex5_word_dictionary.py
+++ learn_Python assignment/Medium3_ex5_word_dictionary.py
@@ -1,7 +1,9 @@
 import PyPDF2
 import string
+import os 
 
-file_handle = open('Sense-and-Sensibility-by-Jane-Austen.pdf','rb')
+path = os.path.dirname(os.path.abspath(__file__))
+file_handle = open(path + '/' + 'Sense-and-Sensibility-by-Jane-Austen.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(file_handle)
 page_number = pdfReader.numPages# this tells you total pages
 
@@ -13,15 +15,17 @@ for page in range(page_number):
     #直接判断linel里的word里的每一个element是不是都是letter
     for line in page_text.split('\n'):
         for word in line.split():
-            letter_list = []
-            for letter in word:
-                if letter.isalpha():
-                    letter_list.append(letter)
-                else:
-                    #没有它，如果word里全是数字，join()函数运行完就会返回''
-                   continue
-
-                real_word = ''.join(letter_list)
+            if word != 'CHAPTER':
+                letter_list = []
+                for letter in word:
+                    if letter.isalpha():
+                        letter_list.append(letter)
+                    else:
+                        #没有它，如果word里全是数字，join()函数运行完就会返回''
+                        continue
+            else:
+                continue
+            real_word = ''.join(letter_list)
            #method2
            #real_word = ''.join(letter for letter in word if letter.isalpha())
 
