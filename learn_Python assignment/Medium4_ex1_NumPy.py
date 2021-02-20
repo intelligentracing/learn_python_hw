45d4906ae161a2ac9bfc08a52f43b07bc5fec8c8
HMTJYQS <13082207660@163.com>
2021-02-20 10:39:36 +0800

    Update Medium4_ex1_NumPy.py

--- learn_Python assignment/Medium4_ex1_NumPy.py
+++ learn_Python assignment/Medium4_ex1_NumPy.py
@@ -19,14 +19,13 @@ def swap_rows(M, a, b):
     return M_1
 
 #m = np.array([[[1,2,3],[3,4,5],[5,6,7]],[[11,2,3],[3,14,5],[5,61,7]],[[1,2,13],[3,4,25],[5,6,27]]])
-m = np.array([[1,2,3],[3,4,5],[5,6,7]])
-print(m)
-print(swap_rows(m,2,3))
 
 def swap_cols(M, a, b):
     #saint check    
     if type(M) is not np.ndarray:
         raise TypeError('M should be matrix type！')
+    if a == b:
+        return M
     if min(a, b) < 1:
         print('Error! There is no negative row index in a matrix')
         return None
@@ -43,4 +42,9 @@ def swap_cols(M, a, b):
     return M_1
 
 m = np.array([[1,2,3],[3,4,5],[5,6,7]])
+print('original matrix:')
+print(m)
+print('swap_rows:')
+print(swap_rows(m,2,3))
+print('swap_cols:')
 print(swap_cols(m,2,3))
