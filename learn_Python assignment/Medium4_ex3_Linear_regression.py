b609b1242906fe6610b3ccea97a732ee8a57bc37
HMTJYQS <13082207660@163.com>
2021-02-22 09:30:51 +0800

    Update Medium4_ex3_Linear_regression.py

--- learn_Python assignment/Medium4_ex3_Linear_regression.py
+++ learn_Python assignment/Medium4_ex3_Linear_regression.py
@@ -1,51 +1,55 @@
 import os
+import datetime
 import numpy as np 
-from sklearn.model_selection import train_test_split
+import math
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+#读取文件
 path = os.path.dirname(os.path.abspath(__file__))
 source_filename = 'AAPL_M4.csv'
 source_handle = open(path+'/'+source_filename,'r')
 
+#将文件中的日期和收盘价作为键值对存放在closing_price_dictionary中
 closing_price_dictionary = dict()
 for line in source_handle.readlines()[1:]:
     line_list = line.split(',')
-    closing_price_dictionary[line_list[6]] = line_list[0]
+    closing_price_dictionary[line_list[0]] = line_list[6]
 
- 
-y_sample = np.zeros(365)
-A_sample = np.zeros((365, 2))
-
+#将字符串格式的日期转换为日期格式，
+# 解决股市周末不开盘总天数达不到365天的问题
+start = ''
 key_list = [] 
-for key in closing_price_dictionary:
-    for j in range(365):
-        key_list.append(key) 
-        A_sample[j] = [j,1]
-for i in range(len(key_list)-365, len(key_list)):
-    for k in range(365):
-        y_sample[k] = key_list[i]
+#用于区分start日期和之后的日期
+n = 0
+for key,value in closing_price_dictionary.items():
+    if n == 0:
+        start = datetime.datetime.strptime(key,'%Y-%m-%d')
+        key_list.append(n) 
+        n += 1
+    else:
+        current = datetime.datetime.strptime(key,'%Y-%m-%d')
+        diff = current - start
+        key_list.append(diff.days)
 
-X = np.array(key_list[-365:]).reshape(-1, 1)
-print(X.shape)
-y = y_sample.reshape(-1, 1)
-print(y.shape)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+x = np.array(key_list).reshape(-1, 1)
+#print(X.shape)
+y = np.array(list(closing_price_dictionary.values()))
+#print(y.shape)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LinearRegression()
+model.fit(x, y)
 
-print(regressor.coef_)
-print(regressor.intercept_)
+print('模型系数a: ',model.coef_)
+print('模型截距b: ',model.intercept_)
+  
+y_pred = model.predict([[365]])
+y_pred_30 = model.predict([[365+30]])
+print('今天的股价是：',y_pred)
+print('30天之后的股价是: ', y_pred_30)
+rms = mean_squared_error(np.array(y[-1]), y_pred)
+#预测数据与原始数据均值之差的平方和/原始数据和原始数据均值之差的平方和
+#print('模型的衡量指标Squared：',rms)
 
-y_pred = regressor.predict(X_test)
 
-rms = sqrt(mean_squared_error(y_test, y_pred))
-print(rms)
 
-# aa = np.linalg.lstsq(A_sample, y_sample, rcond = None)[0]
-# print(aa)
-
-# def price_estimate(x):
-#     return aa[0]*x + aa[1]
-
-# print(price_estimate(395))
